c44d3722296630987df08e12f31bf8aa5e0bcc90
Albert Anthony <albert.anthony1999@gmail.com>
2019-08-15 04:40:52 -0500

    Links and innerText are set to their respected slide images.

--- src/Showcase.js
+++ src/Showcase.js
@@ -3,14 +3,20 @@ import {Link} from "react-router-dom";
 import './Showcase.css';
 import playPause from './playPause.png';
 
-var slideIndex, slides, navBtns, captionText, timer;
+var slideIndex, slides, navBtns, captionText, buttonText, timer;
 class Showcase extends Component{
     constructor(props){
         super(props);
+        this.state={
+            url: ["/amos", "/smallsat", "/astronauts"],
+            num: null
+        };
         this.moveSlide = this.moveSlide.bind(this);
         this.plusSlides = this.plusSlides.bind(this);
         this.setTimer = this.setTimer.bind(this);
         this.playPauseSlides = this.playPauseSlides.bind(this);
+        this.resetTimer = this.resetTimer.bind(this);
+        this.deactivateTimer = this.deactivateTimer.bind(this);
     }
     componentDidMount(){
         this.initGallery();
@@ -24,13 +30,16 @@ class Showcase extends Component{
         captionText = document.querySelector('.captionHolder .captionText');
         captionText.innerText = slides[slideIndex].querySelector(".captionText").innerText;
 
+        buttonText = document.querySelector('.captionHolder .buttonText');
+        buttonText.innerText = slides[slideIndex].querySelector(".buttonText").innerText;
+
         navBtns=[];
         var navBtnsContainer = document.getElementById('navBtnsContainer');
 
         for(let i=0; i < slides.length; i++){
             var navBtn = document.createElement("span");
             navBtn.classList.add("navBtns");
-            navBtn.addEventListener("click", () => {this.moveSlide(i)});
+            navBtn.addEventListener("click", () => {this.moveSlide(i); this.resetTimer();});
             navBtnsContainer.append(navBtn);
             navBtns.push(navBtn);
         };
@@ -75,7 +84,15 @@ class Showcase extends Component{
         captionText.style.display="none";
         captionText.className="captionText "+slideTextAnimClass;
         captionText.innerText=slides[n].querySelector(".captionText").innerText;
-        captionText.style.display="inline-block"
+        captionText.style.display="inline-block";
+
+        buttonText.style.display="none";
+        buttonText.className="buttonText "+slideTextAnimClass;
+        buttonText.innerText=slides[n].querySelector(".buttonText").innerText;
+        buttonText.style.display="inline-block";
+        this.setState({
+            num: n
+        });
     }
     timer = null;
     setTimer(){
@@ -94,31 +111,41 @@ class Showcase extends Component{
             playPauseBtn.style.backgroundPositionY = "-5rem";
         }
     }
+    resetTimer(){
+        clearInterval(timer);
+        this.setTimer();
+    }
+    deactivateTimer(){
+        clearInterval(timer);
+    }
     render(){
         return(
             <section id="Showcase"> {/*CONTAINS ALL SLIDING IMAGES*/}
-                    <div id="right" onClick={() => this.plusSlides(1)}>NEXT&gt;</div>
-                    <div id="left" onClick={() => this.plusSlides(-1)}>&lt;PREV</div>
+                    <div id="right" onClick={() => {this.plusSlides(1); this.resetTimer()}}>NEXT&gt;</div>
+                    <div id="left" onClick={() => {this.plusSlides(-1); this.resetTimer()}}>&lt;PREV</div>
                     <div id="playPauseBtn" onClick={() => this.playPauseSlides()}></div>
 
                 <div className="captionHolder">
                     {/* <p className="captionText"></p> */}
-                    <Link exact to="/amos" className="captionText"></Link>
-                    <Link exact to="/amos">Watch Replay</Link>
+                    <Link exact to={this.state.url[this.state.num]} onClick={() => this.deactivateTimer()} className="captionText"></Link>
+                    <Link exact to={this.state.url[this.state.num]} onClick={() => this.deactivateTimer()} className="buttonText"></Link>
                 </div>
                 <div className="imageHolder"> {/*HOLDS ALL IMAGES HERE*/}
                     <img src="https://www.spacex.com/sites/spacex/files/amos17_v2.jpg" alt="Amos-17 Mission"/>
                     <p className="captionText">Amos-17 Mission</p>
+                    <p className="buttonText">Watch Replay</p>
                 </div>
 
                 <div className="imageHolder"> {/*HOLDS ALL IMAGES HERE*/}
                     <img src="https://www.spacex.com/sites/spacex/files/v2_smallsatheader.png" alt="RideShare Missions"/>
                     <p className="captionText">Smallsat Rideshare Program</p>
+                    <p className="buttonText">Learn More</p>
                 </div>
 
                 <div className="imageHolder"> {/*HOLDS ALL IMAGES HERE*/}
                     <img src="https://www.spacex.com/sites/spacex/files/nasa_astronauts3.jpg" alt="NASA Astronauts on crew Dragon"/>
                     <p className="captionText">NASA Astronauts on Crew Dragon</p>
+                    <p className="buttonText">Learn More</p>
                 </div>
 
                 <div id="navBtnsContainer"></div>
